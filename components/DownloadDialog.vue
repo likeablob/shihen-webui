6148e0f0a229472f9a91287b1f75aa7257b3905d
likeablob <46628917+likeablob@users.noreply.github.com>
2019-10-20 22:18:17 +0900

    Modify offsets.bin format to include the last value

--- components/DownloadDialog.vue
+++ components/DownloadDialog.vue
@@ -122,7 +122,6 @@ export default {
         )
       )
         .drop()
-        .dropRight()
         .value()
 
       console.log(
--- components/DownloadDialogVideo.vue
+++ components/DownloadDialogVideo.vue
@@ -139,7 +139,6 @@ export default {
         )
       )
         .drop()
-        .dropRight()
         .value()
 
       console.log(
